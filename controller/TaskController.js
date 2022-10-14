368c061f55adbf30175f4bdf3aa6f82e0c81aa33
Sabrina Berno <sabrinaberno@hotmail.com>
2022-10-14 20:00:40 -0300

    Componetização no EJS

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,10 +1,16 @@
 const Task = require("../models/Task");
 let message = "";
-let type = ""; 
+let type = "";
 
 const getAllTasks = async (req, res) => {
   try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
+
     const taskList = await Task.find();
+
+    console.log(taskList);
     return res.render("index", {
       taskList,
       task: null,
@@ -21,11 +27,15 @@ const createTask = async (req, res) => {
   const task = req.body;
 
   if (!task.task) {
+    message = "Insira um texto antes de adicionar uma tarefa ";
+    type = "danger";
     return res.redirect("/");
   }
 
   try {
     await Task.create(task);
+    message = "Tarefa criada com sucesso";
+    type = "sucess";
     return res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -52,6 +62,8 @@ const updateOneTask = async (req, res) => {
   try {
     const task = req.body;
     await Task.updateOne({ _id: req.params.id }, task);
+    message = "Tarefa atualizada com sucesso";
+    type = "sucess";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -61,6 +73,21 @@ const updateOneTask = async (req, res) => {
 const deleteOneTask = async (req, res) => {
   try {
     await Task.deleteOne({ _id: req.params.id });
+    message = "Tarefa deletada com sucesso";
+    type = "sucess";
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+
+    task.check ? (task.check = false) : (task.check = true);
+
+    await Task.updateOne({ _id: req.params.id }, task);
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -73,4 +100,5 @@ module.exports = {
   getById,
   updateOneTask,
   deleteOneTask,
+  taskCheck,
 };
